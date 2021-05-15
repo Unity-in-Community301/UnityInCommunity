b447b880e4491854d5a9f3f6162611be9304bb76
Von <aomundia@student.hau.edu.ph>
2021-05-15 14:49:56 +0800

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+const routes: Routes = [
+  {path:'Home',component: HomeComponent},
+  {path:'About',component: AboutComponent},
+  {path:'Project',component: ProjectsComponent},
+  {path:'Contact',component: ContactComponent},
+  {path:'Footer',component: FooterComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,88 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+//Slider
+import {MatSliderModule} from '@angular/material/slider';
 
+//Autocomplete
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+
+//checkbox
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
+//date picker
+import{MatDatepickerModule} from '@angular/material/datepicker';
+
+//importing native date module(moved to core in angular 11)
+import {MatNativeDateModule} from  '@angular/material/core';
+
+//Select module
+import {MatSelectModule} from '@angular/material/select';
+
+//MatOptionModule moved to core
+import {MatOptionModule} from '@angular/material/core';
+
+//badge
+import {MatBadgeModule} from '@angular/material/badge';
+
+//menu
+import {MatMenuModule} from '@angular/material/menu';
+
+//HTTP client
+import {HttpClientModule} from '@angular/common/http';
+
+//Grid
+import {MatGridListModule} from '@angular/material/grid-list';
+
+//Divider
+import {MatDividerModule} from '@angular/material/divider';
+
+//Radio
+import {MatRadioModule} from '@angular/material/radio';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSliderModule,
+    MatAutocompleteModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatIconModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatBadgeModule,
+    MatMenuModule,
+    MatButtonModule,
+    HttpClientModule,
+    HttpClientModule,
+    MatGridListModule,
+    MatDividerModule, 
+    MatRadioModule,
+    MatFormFieldModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
